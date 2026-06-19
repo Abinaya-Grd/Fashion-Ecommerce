76b2f7b700f638a0889910bbb855059a28dfbcb4
Abinaya-Grd <23bct002@grd.edu.in>
2026-06-19 09:45:12 +0530

    settings

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myproject_db',
         'USER': 'root',
-        'PASSWORD': 'abisiddhu',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
